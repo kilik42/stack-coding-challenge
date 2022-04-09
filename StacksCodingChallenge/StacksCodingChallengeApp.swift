97e98cda11d168b826230e68af3be9ceda7c92ed
marvin evins <marvinevins@yahoo.com>
2022-04-09 11:48:31 -0500

    Initial Commit

---
+++ StacksCodingChallenge/StacksCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StacksCodingChallengeApp.swift
+//  StacksCodingChallenge
+//
+//  Created by marvin evins on 4/9/22.
+//
+
+import SwiftUI
+
+@main
+struct StacksCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
